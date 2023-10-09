0320f214b80d7133cdf5a049e1026bc551a77a4e
Asif359 <asifahammednishst@gmail.com>
2023-10-09 11:58:13 +0600

    update Profile page

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../AuthProviders/AuthProvider';
 
 const Profile = () => {
+    const {user} = useContext(AuthContext)
+    console.log(user);
     return (
-        <div>
-            profile
+        <div className='p-3'>
+            <div className='grid grid-cols-1 gap-2'>
+                <div>
+                    <img className='w-96' src={user.photoURL} alt="" />
+                </div>
+                <div>
+                    <h1><span className='font-bold'> Name :</span>{user.displayName}</h1>
+                    <h1><span className='font-bold'> Email :</span>{user.email}</h1>
+                </div>
+            </div>
         </div>
     );
 };
